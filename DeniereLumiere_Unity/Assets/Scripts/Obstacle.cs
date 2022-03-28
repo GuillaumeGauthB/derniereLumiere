2415c545e1b6c19ffd15fff75732396f514fa5dc
Jonathan Mores <jonathanmopro@gmail.com>
2022-03-28 11:35:35 -0400

    Ajout des script de Guillaume

---
+++ DeniereLumiere_Unity/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Joueur_Script>().estDash)
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            Invoke("MettreCollider", 0.5f);
+        }
+    }
+
+    void MettreCollider()
+    {
+        gameObject.GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+
+}
