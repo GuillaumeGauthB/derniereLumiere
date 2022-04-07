25074b044544d2e2d8486f2568d5a2cd3e83b0cf
Jonathan Mores <jonathanmopro@gmail.com>
2022-04-07 15:54:26 -0400

    Update Joueur_Script.cs
    Changement merge fini

--- DeniereLumiere_Unity/Assets/Scripts/Joueur_Script.cs
+++ DeniereLumiere_Unity/Assets/Scripts/Joueur_Script.cs
@@ -7,57 +7,64 @@ using UnityEngine.InputSystem;
 /*Dernière modification 2022-03-09*/
 public class Joueur_Script : MonoBehaviour
 {
+    /* Script principal du personnage
+      Par : Jonathan Mores et Guillaume Gauthier-Benoit
+      Dernière modification : 03/04/2022
+    */
+
     /***********GameObject***********/
-    public GameObject sprite;
+    public GameObject sprite; // Le sprite du personnage
 
     /***********Bool***********/
-    private bool b_estAuSol;
-    private bool course;
-    public bool accroupir;
-    private bool b_JoueurSurPlateforme;
+    private bool b_estAuSol; // Variable boolean pour determiner si le personnage est au sol
+    private bool course; // Variable pour determiner si le personnage est en mode de course
+    public bool accroupir; // Variable pour determiner si le personnage est en mode accoupit
+    private bool b_JoueurSurPlateforme; // Variable pour determiner si le personnage est sur une plateforme
 
     /********Raccourcis********/
-    private Rigidbody2D rb_Joueur;
-    private InputJoueur i_inputJoueur;
-    private Animator a_Joueur;
+
+    private Rigidbody2D rb_Joueur; // le rigidbody du joueur
+    private InputJoueur i_inputJoueur; // Le player input du joueur
+    private Animator a_Joueur; // L'Animator du Sprite du joueur
+
 
     /********Transform********/
-    public Transform checkSol;
+    public Transform checkSol; // La position pour determinee si le joueur est au sol
 
     /********Layer Mask********/
-    public LayerMask solLayer;
+    public LayerMask solLayer; // La layer du sol111
 
     /********Float********/
-    public float forceSaut;
-    public float vitesseAcceleration;
-    public float vitesseMaximaleMarche;
-    public float vitesseMaximaleCourrir;
-    private float f_vitesseMaximale;
+    public float forceSaut; // La force du saut
+    public float vitesseAcceleration; // La vitesse d'acceleration du deplacement 
+    public float vitesseMaximaleMarche; // La vitesse maximale de la marche
+    public float vitesseMaximaleCourrir; // La vitesse maximale de la course
+    private float f_vitesseMaximale; // La vitesse maximale de la marche ou de la course
 
-    /********Variabe a Guillaume********/
-    public static bool b_doubleSautObtenu = true,
+    /********Variables a Guillaume********/
+    public static bool b_doubleSautObtenu = true, // Variables determinant quels pouvoirs sont obtenus
         b_dashObtenu = true,
         b_stunObtenu = true,
         b_tirObtenu;
-    private bool b_doubleSautPossible,
+    private bool b_doubleSautPossible, // Variables determinant si les differents pouvoirs peuvent etre utilisers
         b_dashPossible = true,
         b_stunPossible;
 
-    public float forceDash,
+    public float forceDash, // Variables publics pertinentes pour l'utilisation du dash
         commencerTimerDash,
         presentTimerDash;
 
-    public bool estDash;
+    public bool estDash; // Variable determinant si le personnage est en cours de dash ou pas
 
-    private float f_movX,
+    private float f_movX, // Variables privees pertinentes pour l'utilisation du dash
         f_directionDash,
         f_cooldownDash = 1;
 
-    private Vector3 checkpoint;
-
+    private Vector3 checkpoint; // La position du checkpoint
 
     void Awake()
     {
+        // Assignation des variables et des c# events
         a_Joueur = sprite.GetComponent<Animator>();
         rb_Joueur = GetComponent<Rigidbody2D>();
         f_vitesseMaximale = vitesseMaximaleMarche;
@@ -69,37 +76,32 @@ public class Joueur_Script : MonoBehaviour
     }
     private void Update()
     {
-        b_estAuSol = Physics2D.OverlapCircle(checkSol.position, 0.3f, solLayer);
+        // Verifier si le personnage est sur le sol
+        b_estAuSol = Physics2D.OverlapCircle(checkSol.position, 0.5f, solLayer);
+
         /* ================================================================================= Modifications Guillaume =====================================*/
+
         // Si le personnage est au sol, permettre un double saut
         if (b_estAuSol)
         {
             a_Joueur.SetBool("estAuSol", true);
             b_doubleSautPossible = true;
         }
-        else
-        {
-            a_Joueur.SetBool("estAuSol", false);
-        }
-        /* ================================================================================= Fin Modifs Guillaume    =====================================*/
+
     }
 
     private void FixedUpdate()
     {
-        //GetComponent<PlayerInput>().currentActionMap.ToString() != "TirLucioles"
         if (!estDash && !GetComponent<Inputs_Guillaume>().declencherTir)
         {
             /* permet de lire le input du new input system*/
             Vector2 inputVector = i_inputJoueur.Player.Mouvement.ReadValue<Vector2>();
             rb_Joueur.AddRelativeForce(new Vector2(inputVector.x * vitesseAcceleration, 0f), ForceMode2D.Impulse);
-
-            /* permet au personnage de ne pas dÃ©passer sa vitesse maximale*/
-
+            /* permet au personnage de ne pas dépasser sa vitesse maximale*/
             if (rb_Joueur.velocity.x > f_vitesseMaximale || rb_Joueur.velocity.x < -f_vitesseMaximale)
             {
                 rb_Joueur.velocity = new Vector2(inputVector.x * f_vitesseMaximale, rb_Joueur.velocity.y);
                 sprite.GetComponent<Animator>().SetBool("Course", true);
-
             }
             else
             {
@@ -113,8 +115,7 @@ public class Joueur_Script : MonoBehaviour
             {
                 sprite.GetComponent<SpriteRenderer>().flipX = false;
             }
-
-            /*ligne pour voir si le personnage ce dÃ©place*/
+            /*ligne pour voir si le personnage ce déplace*/
             if (rb_Joueur.velocity.magnitude > 0)
             {
                 /*Debug.Log(rb_Joueur.velocity);*/
@@ -126,14 +127,13 @@ public class Joueur_Script : MonoBehaviour
         // Si le personnage est en train de dash...
         if (estDash)
         {
-
-            // Le dÃ©placer et faire diminuer le temps
+            // Le déplacer et faire diminuer le temps
             rb_Joueur.velocity = transform.right * f_directionDash * forceDash;
             presentTimerDash -= Time.deltaTime;
-            // Si le temps est Ã©gal Ã  zÃ©ro...
+            // Si le temps est égal à zéro...
             if (presentTimerDash <= 0)
             {
-                // ArrÃªter le dash et commencer le cooldown
+                // Arrêter le dash et commencer le cooldown
                 estDash = false;
                 InvokeRepeating("Cooldown", 0, 1f);
             }
@@ -141,30 +141,31 @@ public class Joueur_Script : MonoBehaviour
         // Si le cooldown vaut moins que 0, que le personnage ne peut dash et a le pouvoir...
         if (f_cooldownDash <= 0 && !b_dashPossible && b_dashObtenu)
         {
-
-            // Rendre le dash possible, rÃ©initialiser le temps de cooldown et arrÃªter de le faire descendre
-
+            // Rendre le dash possible, réinitialiser le temps de cooldown et arrêter de le faire descendre
             b_dashPossible = true;
             f_cooldownDash = 1;
             CancelInvoke("Cooldown");
         }
-
-        //Debug.Log(f_cooldownDash);
-        /* ================================================================================= Fin Modifs Guillaume    =====================================*/
     }
 
+    // La fonction gerant l'activation du saut
     void Saut(InputAction.CallbackContext context)
     {
+        // Lorsque l'action est performee...
         if (context.performed)
         {
-            if (b_estAuSol == true && !accroupir && b_doubleSautPossible && b_doubleSautObtenu)
+            // si le personnage est au sol et n'est pas accroupit...
+            if (b_estAuSol == true && !accroupir && !GetComponent<Inputs_Guillaume>().declencherTir)
             {
+                // Faire sauter le personnage
                 rb_Joueur.AddForce(new Vector2(0, 1 * forceSaut));
                 a_Joueur.SetTrigger("Saut");
                 a_Joueur.SetBool("Atteri", false);
             }
+            // Si le personnage n'est pas sur le sol et peut faire un double saut...
             else if (!b_estAuSol && b_doubleSautPossible && b_doubleSautObtenu)
             {
+                // Faire sauter le personnage et empecher de faire un autre saut
                 rb_Joueur.AddForce(new Vector2(0, 1 * forceSaut));
                 b_doubleSautPossible = false;
                 a_Joueur.SetTrigger("Saut");
@@ -173,28 +174,40 @@ public class Joueur_Script : MonoBehaviour
         }
     }
 
+    // La fonction gerant la course du personnage
+
     void Courrir(InputAction.CallbackContext context)
     {
+        // Lorsque l'action est performee
         Debug.Log(context.phase);
         if (context.performed)
         {
+            // Si le personnage n'est pas en mode de course
             if (course == false)
             {
+                // Changer la vitesse maximale et activer la variable de course
                 f_vitesseMaximale = vitesseMaximaleCourrir;
                 course = true;
             }
+            // Sinon
             else
             {
+                // Changer la vitesse maximale et desactiver la course
                 f_vitesseMaximale = vitesseMaximaleMarche;
                 course = false;
             }
         }
     }
 
+    // Fonction gerant l'accroupissement du personnage
     void Accroupir(InputAction.CallbackContext context)
     {
+
+        // Si l'action est performee...
+        Debug.Log(context.phase);
         if (context.performed)
         {
+            // Activer ou desactiver l'accroupissement
             if (accroupir == false)
             {
                 accroupir = true;
@@ -206,16 +219,14 @@ public class Joueur_Script : MonoBehaviour
         }
     }
 
-
-    // Fonction gÃ©rant les dash
+    // Fonction gérant les dash
     public void Dash(InputAction.CallbackContext context)
     {
         Debug.Log(f_movX);
-        // Si le bouton est appuyÃ©, qu'il peut dash, que le pouvoir est obtenu, et que son mouvement sur l'axe des x n'est pas 0...
-
+        // Si le bouton est appuyé, qu'il peut dash, que le pouvoir est obtenu, et que son mouvement sur l'axe des x n'est pas 0...
         if (context.started && b_dashPossible && b_dashObtenu && f_movX != 0)
         {
-            //le faire dash
+            //faire le dash
             estDash = true;
             presentTimerDash = commencerTimerDash;
             rb_Joueur.velocity = Vector2.zero;
@@ -224,19 +235,15 @@ public class Joueur_Script : MonoBehaviour
         }
     }
 
-
-    // Fonction gÃ©rant les cooldowns des pouvoirs (va devoir Ãªtre retravaillÃ©e)
-
+    // Fonction gérant les cooldowns des pouvoirs (va devoir être retravaillée)
     void Cooldown()
     {
+        // Si le cooldown du dash est plus grand ou egal a 0 et que le dash n'est pas possible 
         if (!b_dashPossible && f_cooldownDash >= 0)
         {
+            // Diminuer la valeur du cooldown de dash
             f_cooldownDash -= 1;
         }
-        /*if (!b_dashPossible)
-        {
-            b_dashPossible = true;
-        }*/
     }
     /*void Courrir(InputAction.CallbackContext context)
     {
@@ -252,9 +259,7 @@ public class Joueur_Script : MonoBehaviour
         }
     }*/
 
-
-    // Fonction dÃ©tectant les collisions
-
+    // Fonction détectant les collisions
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Si le personnage entre en collision avec un checkpoint, sauvegarder sa position
@@ -268,12 +273,12 @@ public class Joueur_Script : MonoBehaviour
             transform.position = checkpoint;
         }
     }
-
-   private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (b_estAuSol == true)
         {
             a_Joueur.SetBool("Atteri", true);
         }
     }
+
 }
