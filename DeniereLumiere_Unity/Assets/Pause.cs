750dc58abfae41455b30bc7db168c8dc8e358408
Jérôme Trottier <jerome.trottier@hotmail.ca>
2022-04-25 09:51:12 -0400

    Menu pause régler et modifications des crédits

--- DeniereLumiere_Unity/Assets/Pause.cs
+++ DeniereLumiere_Unity/Assets/Pause.cs
@@ -1,24 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Pause : MonoBehaviour
 {
+    /**
+     * Classe qui permet de gerer tout ce qui touche au menu pause du jeu
+     * Codeurs : Jerome
+     * Derniere modification : 25/04/2022
+    */
+    //Particules du menu pause
+    public GameObject particules;
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        // Quand on appuie sur Escape, le jeu est mit en pause
+        if (Input.GetKey(KeyCode.Escape))
         {
             PauserJeu();
         }
     }
+
+    // Fonction qui permet de pauser le jeu et d'afficher le menu pause
     public void PauserJeu()
     {
+        GetComponent<Animator>().SetBool("EnPause", true);
         Time.timeScale = 0;
-        gameObject.SetActive(true);
-
+        Cursor.visible = true;
     }
+    // Fonction qui permet de depauser le jeu et d'enlever le menu pause
     public void DepauserJeu()
     {
+        GetComponent<Animator>().SetBool("EnPause", false);
         Time.timeScale = 1;
+    }
+    // Fonction qui permet d'activer les particules du menu pause (particules de hover)
+    public void activerParticules()
+    {
+        particules.SetActive(true);
+    }
+    // Fonction qui permet de desactiver les particules du menu pause
+    public void desactiverParticules()
+    {
+        particules.SetActive(false);
     }
 }
