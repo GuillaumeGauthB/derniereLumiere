e9dc5212aca4e825398770a846a611c7494ccd1e
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2022-05-14 20:38:06 -0400

    Fin du boss loup

--- DeniereLumiere_Unity/Assets/Scripts/Ai/Boss/BossLoup/LuciolesBoss.cs
+++ DeniereLumiere_Unity/Assets/Scripts/Ai/Boss/BossLoup/LuciolesBoss.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class LuciolesBoss : MonoBehaviour
 {
-    private float vitesse = 3;
-    private GameObject beepo;
-    // Start is called before the first frame update
+    /** Script de gestion des lucioles ennemies du boss
+     * Créé par Guillaume Gauthier-Benoît
+     * Dernière modification: 09/05/22
+     */
+
+    private float vitesse = 3; // la vitesse des lucioles
+    private GameObject beepo; // la cible des lucioles
+    
     void Start()
     {
-        beepo = GameObject.Find("Beepo");
-        Invoke("Destruction", 8f);
+        beepo = GameObject.Find("Beepo"); // choisir le joueur comme cible
+        Invoke("Destruction", 4f); // appeler Destruction() apres 4s
     }
 
     // Update is called once per frame
     void Update()
     {
+        // faire deplacement les lucioles en direction de la cible
         gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, beepo.transform.position, vitesse * Time.deltaTime);
     }
 
+    // la fonction gerant la destruction des lucioles
     void Destruction()
     {
+        // detruire la luciole
         Destroy(gameObject);
     }
 }
