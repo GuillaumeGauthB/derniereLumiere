7dd52c726ce39ff3f8d2a677713b5c98bc0d8da7
Jérôme Trottier <81482137+JeromeTrottier@users.noreply.github.com>
2022-05-20 11:19:50 -0400

    Commentaires BoutonSelect.cs

--- DeniereLumiere_Unity/Assets/Scripts/Interface/BoutonSelect.cs
+++ DeniereLumiere_Unity/Assets/Scripts/Interface/BoutonSelect.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 using UnityEngine.EventSystems; // Librairie besoin pour utiliser les evenements unity
 
 /**
-     * Classe qui permet de gerer tout ce qui aux boutons de jeu
+     * Classe qui permet de gerer tout ce qui touche aux boutons de jeu
      * Codeurs : Jerome
      * Derniere modification : 25/04/2022
     */
-public class BoutonSelect : MonoBehaviour, ISelectHandler, IPointerEnterHandler, IMoveHandler, ISubmitHandler // Interfaces requisent pour utiliser la fonction OnSelect() et onDeselect()
+// La classe herite de plusieurs interfaces requisent pour detecter quand le joueur select un bouton, pointe un bouton, change de bouton et clique sur un bouton
+public class BoutonSelect : MonoBehaviour, ISelectHandler, IPointerEnterHandler, IMoveHandler, ISubmitHandler 
 {
+    [Header("Particules menu")]
     public GameObject particules;
+    [Header("Sons")]
     public AudioSource audioCanvas;
     public AudioClip sonMove;
     public AudioClip sonSelect;
 
+    // Variable qui determine si le bouton va etre select quand le menu sera ouvert pour la premiere fois
     public bool firstSelected = false;
 
+    //Methode qui selectionne le bouton avec firstSelected de cocher dans l'inspecteur
     public void ActiverBoutons()
     {
         if (firstSelected)
@@ -24,41 +29,46 @@ public class BoutonSelect : MonoBehaviour, ISelectHandler, IPointerEnterHandler,
             setBoutonSelect(gameObject);
         }
     }
+
+    // Methode qui selectionne un bouton automatiquement a la place du joueur
     public void setBoutonSelect(GameObject boutonParDefaut)
     {
         GameObject eventSystem = GameObject.FindGameObjectWithTag("eventSystem");
         eventSystem.GetComponent<EventSystem>().SetSelectedGameObject(boutonParDefaut);
     }
 
+    // Methode qui herite de IPointerEnterHandler pour detecter quand le joueur pointe avec le curseur un bouton 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        // Selectionner le bouton pointé
         setBoutonSelect(gameObject);
-        entrerSelect();
+        // On change la position des particules
+        envoyerPositionParticules();
+        // Jouer le son de selection
         audioCanvas.PlayOneShot(sonMove);
     }
 
+    // Quand le joueur navigue avec les touches de clavier ou boutons manettes, la methode OnMove est appelee
     public void OnMove(AxisEventData eventData)
     {
+        // Jouer le son de navigation
         audioCanvas.PlayOneShot(sonMove);
     }
 
     // Fonction de Unity.EventSystems qui permet de détecter lorsque le gameobject est selectionné
     public void OnSelect(BaseEventData eventData)
     {
-        entrerSelect();
+        // Quand un bouton est selectionne, on change la position des particules
+        envoyerPositionParticules();
     }
 
     public void OnSubmit(BaseEventData eventData)
     {
+        // Quand le joueur appuie sur un bouton, on fait jouer le son de selection du bouton
         audioCanvas.PlayOneShot(sonSelect);
     }
-    // Active les icones lorsque la fonction est appelée
-    public void entrerSelect()
-    {
-        envoyerPositionParticules();
-    }
 
-    // Trouve les icones et les activent
+    // Fonction qui permet de changer la position des particules a celle du bouton
     private void envoyerPositionParticules()
     {
         if (particules.activeInHierarchy) particules.GetComponent<ParticuleSystemeMenu>().setNouvellePosition(transform.position.y);
