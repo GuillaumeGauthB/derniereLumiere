5d1a1dc0608d755d1f5b35c6ef044d7aa71aa1f7
Jérôme Trottier <81482137+JeromeTrottier@users.noreply.github.com>
2022-05-20 10:41:53 -0400

    Script pour gerer la musique

--- DeniereLumiere_Unity/Assets/Scripts/Ai/Boss/ScriptBossRoom.cs
+++ DeniereLumiere_Unity/Assets/Scripts/Ai/Boss/ScriptBossRoom.cs
@@ -4,34 +4,30 @@ using UnityEngine;
 
 public class ScriptBossRoom : MonoBehaviour
 {
+    [Header("Le Boss")]
     public GameObject Boss;
+    [Header("UI")]
     public GameObject panelBossUI;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Musique")]
+    public GameObject musiqueController;
+    public AudioClip musiqueBoss;
+    public AudioClip musiqueIdle;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
+            musiqueController.GetComponent<MusiqueController>().SetNouvelleMusique(musiqueBoss);
             Boss.SetActive(true);
             panelBossUI.SetActive(true);
             Destroy(gameObject, 2f);
         }
-        
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
+            musiqueController.GetComponent<MusiqueController>().SetNouvelleMusique(musiqueIdle);
             panelBossUI.SetActive(false);
         }
     }
