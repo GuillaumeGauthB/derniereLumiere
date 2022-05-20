e5a2cfb04b34840fb8c25229d885cad5e1849aec
Jérôme Trottier <81482137+JeromeTrottier@users.noreply.github.com>
2022-05-20 11:06:14 -0400

    Commentaires SceneController.cs

--- DeniereLumiere_Unity/Assets/Scripts/Gameplay_Attaque/Joueur/SceneController.cs
+++ DeniereLumiere_Unity/Assets/Scripts/Gameplay_Attaque/Joueur/SceneController.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
-{ 
+{
+    /** Script de gestion de changement de scenesd
+     * Cree par Jerome Trottier
+     * Derniere modification: 20/05/22
+     */
+
+    // Methode qui permet de changer de scene en lui passant le nom de la scene voulu
     public void changerScene(string nomScene = "MenuJeu")
     {
+        // Reset du timescale a 1, dans le cas ou le joueur est dans un menu pause lorsquil change de scene
         Time.timeScale = 1;
         SceneManager.LoadScene(nomScene);
     }
+
+    // Methode qui permet de fermer le jeu
     public void quitterJeu()
     {
         Application.Quit();
