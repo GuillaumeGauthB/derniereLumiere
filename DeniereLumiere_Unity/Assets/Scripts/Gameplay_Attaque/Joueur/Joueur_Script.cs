22d594989f8d3767c22e147aeb98b52a2eb1662e
Jonathan Mores <jonathanmopro@gmail.com>
2022-05-20 21:00:14 -0400

    Enlever les pouvoirs

--- DeniereLumiere_Unity/Assets/Scripts/Gameplay_Attaque/Joueur/Joueur_Script.cs
+++ DeniereLumiere_Unity/Assets/Scripts/Gameplay_Attaque/Joueur/Joueur_Script.cs
@@ -44,9 +44,9 @@ public class Joueur_Script : MonoBehaviour
     /********Variables a Guillaume********/
     [Header("Variables a Guillaume")]
     public static bool doubleSautObtenu, // Variables determinant quels pouvoirs sont obtenus
-        dashObtenu = true,
+        dashObtenu = false,
         stunObtenu,
-        tirObtenu = true;
+        tirObtenu = false;
     private bool b_doubleSautPossible, // Variables determinant si les differents pouvoirs peuvent etre utilises
         b_dashPossible = true,
         b_stunPossible;
